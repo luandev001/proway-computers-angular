a133557da0b8451b37ca2081a395d92ea0895dcf
Luan Araujo <69086390+luandev001@users.noreply.github.com>
2023-08-28 22:40:11 -0300

    Criação de rota para página não encontrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
